c602dbe57b0b83e53248e837552d1e96669b230c
Sienna <sienna.m.wood@gmail.com>
2018-02-20 14:11:28 -0700

    seed data for boards, lists, cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+board = Board.create(title: 'First Board')
+Board.create(title: 'Second Board')
+Board.create(title: 'Third Board')
+
+list = List.create(title: 'List 1', board: board)
+List.create(title: 'List 2', board: board)
+list3 = List.create(title: 'List 3', board: board)
+
+Card.create(title: 'This is the first card', list: list)
+Card.create(title: 'This is the second card', list: list)
+Card.create(title: 'This is the third card', list: list)
+Card.create(title: 'This is the fourth card', list: list3)
