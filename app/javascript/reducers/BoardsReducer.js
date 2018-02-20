b478701520e711201f4bf0327fd7d0e2cd06a063
Sienna <sienna.m.wood@gmail.com>
2018-02-20 16:27:11 -0700

    refactor reducers to use switch statements

--- app/javascript/reducers/BoardsReducer.js
+++ app/javascript/reducers/BoardsReducer.js
@@ -1,17 +1,19 @@
 export default function boardsReducer(state = [], action) {
-  if (action.type === 'FETCH_BOARDS_SUCCESS') {
-    return action.boards;
-  } else if (action.type === 'CREATE_BOARD_SUCCESS') {
-    const newBoard = action.board;
-    newBoard.id = Number(newBoard.id);
+  switch (action.type) {
+    case 'FETCH_BOARDS_SUCCESS':
+      return action.boards;
 
-    return state.concat(newBoard);
-  } else if (action.type === 'FETCH_BOARD_SUCCESS') {
-    const excludedBoards = state.filter(board => board.id !== action.board.id);
-    const { lists, ...newBoardWithoutLists } = action.board;
+    case 'CREATE_BOARD_SUCCESS':
+      const newBoard = action.board;
+      newBoard.id = Number(newBoard.id);
+      return state.concat(newBoard);
 
-    return excludedBoards.concat(newBoardWithoutLists);
-  } else {
-    return state;
+    case 'FETCH_BOARD_SUCCESS':
+      const excludedBoards = state.filter(board => board.id !== action.board.id);
+      const {lists, ...newBoardWithoutLists} = action.board;
+      return excludedBoards.concat(newBoardWithoutLists);
+
+    default:
+      return state;
   }
 }
