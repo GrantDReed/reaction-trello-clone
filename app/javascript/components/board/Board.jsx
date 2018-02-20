14294e79bc8d2a0a36e611aec25832fe1f319fa3
Sienna <sienna.m.wood@gmail.com>
2018-02-20 13:50:42 -0700

    use self-closing tags for empty components (eslint)

--- app/javascript/components/board/Board.jsx
+++ app/javascript/components/board/Board.jsx
@@ -21,7 +21,9 @@ const Board = props => {
         {/*<ListOfLists boardId={props.board} />*/}
       </main>
       <div className="menu-sidebar">
-        <div id="menu-main" className="main slide"><i className="back-icon icon"></i><i className="x-icon icon"></i>
+        <div id="menu-main" className="main slide">
+          <i className="back-icon icon"/>
+          <i className="x-icon icon"/>
           <h1>Menu</h1>
           <div className="menu-contents">
             <div className="members">
@@ -35,7 +37,9 @@ const Board = props => {
                 <div className="card-member ">KW</div>
               </div>
             </div>
-            <div className="add-members"><i className="add-icon sm-icon"></i>Add Members...</div>
+            <div className="add-members">
+              <i className="add-icon sm-icon"/>Add Members...
+            </div>
             <hr />
             <ul className="menu-list">
               <li className="background-item">Change Background</li>
@@ -47,27 +51,27 @@ const Board = props => {
               <li className="activity-icon menu-icon not-implemented">Activity</li>
             </ul>
             <ul className="activity-list">
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> changed the background of this board <small>yesterday at 4:53 PM</small>
                 </p>
               </li>
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> sent <span className="link">Use the + in the top menu to make your first board now.</span> to the board <small>4 hours ago</small>
                 </p>
               </li>
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> archived <span className="link">Use the + in the top menu to make your first board now.</span> <small>4 hours ago</small>
                 </p>
               </li>
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> changed the background of this board <small>5 hours ago</small>
                 </p>
               </li>
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> changed the background of this board <small>6 hours ago</small>
                 </p>
               </li>
-              <li><i className="member-icon"></i>
+              <li><i className="member-icon"/>
                 <p><span className="member-name">Taylor Peat</span> changed the background of this board <small>yesterday at 10:23 PM</small>
                 </p>
               </li>
@@ -75,8 +79,8 @@ const Board = props => {
           </div>
         </div>
       </div>
-      <div id="modal-container"></div>
-      <div id="dropdown-container"></div>
+      <div id="modal-container"/>
+      <div id="dropdown-container"/>
     </div>
   )
 }
