d0b98b3d00712dd31310da0618af02ed94600369
Sienna <sienna.m.wood@gmail.com>
2018-02-21 14:45:28 -0700

    make boards and Trello logo in TopNav links to root, style accordingly

--- app/javascript/components/shared/TopNav.jsx
+++ app/javascript/components/shared/TopNav.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const TopNav = () => (
   <nav>
-      <ul>
-        <li className="boards trello-icon icon">
-          <span>Boards</span>
-        </li>
-        <li className="search-container">
-          <div className="search search-icon icon"></div>
-          <div className="active-search">
-            <div>
-              <input type="text" /> 
-            </div>
-            <i className="x-icon icon"></i>
-            <i className="goto-icon icon"></i>
+    <ul>
+      <li className="boards trello-icon icon">
+        <NavLink to='/'>Boards</NavLink>
+      </li>
+      <li className="search-container">
+        <div className="search search-icon icon"/>
+        <div className="active-search">
+          <div>
+            <input type="text"/>
           </div>
-        </li>
-      </ul>
-      <h1>Trello</h1>
-      <ul className="user-info">
-        <li className="create-icon icon"></li>
-        <li className="split-button-1">VR</li>
-        <li className="split-button-2">Victor Reyes</li>
-        <li className="info-icon icon"></li>
-        <li className="notifications-icon icon"></li>
-      </ul>
+          <i className="x-icon icon"/>
+          <i className="goto-icon icon"/>
+        </div>
+      </li>
+    </ul>
+    <h1><NavLink to='/'>Trello</NavLink></h1>
+    <ul className="user-info">
+      <li className="create-icon icon"/>
+      <li className="split-button-1">VR</li>
+      <li className="split-button-2">Victor Reyes</li>
+      <li className="info-icon icon"/>
+      <li className="notifications-icon icon"/>
+    </ul>
   </nav>
 );
 
