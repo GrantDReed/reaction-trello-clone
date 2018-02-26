27f94a59fff3e4f5ab4ec12ea12ce935d455786d
Sienna <sienna.m.wood@gmail.com>
2018-02-26 12:13:05 -0700

    add create comment success to current card reducer

--- app/javascript/reducers/CurrentCardReducer.js
+++ app/javascript/reducers/CurrentCardReducer.js
@@ -4,6 +4,8 @@ export default function currentCardReducer(state = {}, action) {
       return action.currentCard;
     case 'UPDATE_CARD_SUCCESS':
       return action.currentCard;
+    case 'CREATE_COMMENT_SUCCESS':
+      return action.currentCard;
     default:
       return state;
   }
