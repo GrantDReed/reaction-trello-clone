a8847fd1755ff4809f58ec582d68b3cf3193c8a9
Sienna <sienna.m.wood@gmail.com>
2018-02-26 10:37:57 -0700

    add comments to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220183600) do
+ActiveRecord::Schema.define(version: 20180226165343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180220183600) do
   create_table "cards", force: :cascade do |t|
     t.string "title"
     t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.string "text"
+    t.integer "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
